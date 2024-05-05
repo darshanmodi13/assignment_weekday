c4efa320c8f7fd05257aeefe7a78a1b18b978c10
darshanmodi <darshanmodi@solvative.com>
2024-05-05 18:51:44 +0530

    feat(redux toolkit query setup): added redux toolkit query

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+
+// MUI
 import { Box } from '@mui/material';
 
+// Redux toolkit query
+import { useGetJobsMutation } from './services/jobs.service';
+
 function App() {
+	const [getJobs, { data }] = useGetJobsMutation();
+	console.log('🚀 ~ App ~ data:', data);
+
+	useEffect(() => {
+		getJobs({ limit: 10, page: 1 });
+	}, []);
 	return (
 		<>
 			<Box component="section" className="container" color="primary.main">
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 import { ThemeProvider } from '@mui/material';
+import { Provider } from 'react-redux';
+
+// Component
+import App from './App.tsx';
+
+// Styles
+import './index.css';
+
+// Theme
 import theme from './themes/theme.ts';
+
+// Store
+import { store } from './store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
+		<Provider store={store}>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</Provider>
 	</React.StrictMode>
 );
